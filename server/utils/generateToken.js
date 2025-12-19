bca4a3df7e92d7dbf5f5ab02dfc502d5149660cf
rahilazhar12 <158289635+rahilazhar12@users.noreply.github.com>
2025-12-19 15:08:33 +0500

    c

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -8,7 +8,8 @@ const generateToken = (payload, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    // sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
     return token; // <-- this line is important
